712a03e8dbd9390b4a2ecceaf37d4deba7f5ad71
flowi0 <gultsev2003@gmail.com>
2024-10-28 21:49:33 +0300

    lab-4: do lab

---
+++ MyWebsite/Scripts/ActivePage.js
@@ -0,0 +1,14 @@
+let name = document.location.pathname.split("/").slice(-1)[0];
+
+if (name === "index.html"){
+    document.getElementById("navigation_list").children[0].className = "active_navigation_list_button_type"
+}
+else if (name === "MyFavouriteFilms.html"){
+    document.getElementById("navigation_list").children[1].className = "active_navigation_list_button_type"
+}
+else if (name === "ToWatchInsertField.html"){
+    document.getElementById("navigation_list").children[2].className = "active_navigation_list_button_type"
+}
+else if (name === "SomePage.html"){
+    document.getElementById("navigation_list").children[3].className = "active_navigation_list_button_type"
+}
---
+++ MyWebsite/Scripts/LoadTimer.js
@@ -0,0 +1,9 @@
+let before_loadTime = new Date().getTime();
+window.onload = PageLoadTime;
+function PageLoadTime() {
+    let after_loadTime = new Date().getTime();
+    // Time calculating in seconds
+    let pageLoadTime = (after_loadTime - before_loadTime) / 1000
+
+    document.getElementById("loadtime").innerHTML = "Page load time is " + pageLoadTime + " Seconds";
+}
