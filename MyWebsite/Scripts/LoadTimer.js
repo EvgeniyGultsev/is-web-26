12f83d8514e7f1c7227f6e54d0b65ecd9e7cd65b
flowi0 <gultsev2003@gmail.com>
2024-10-30 13:20:23 +0300

    little edit

--- MyWebsite/Scripts/LoadTimer.js
+++ MyWebsite/Scripts/LoadTimer.js
@@ -2,7 +2,6 @@ let before_loadTime = new Date().getTime();
 window.onload = PageLoadTime;
 function PageLoadTime() {
     let after_loadTime = new Date().getTime();
-    // Time calculating in seconds
     let pageLoadTime = (after_loadTime - before_loadTime) / 1000
 
     document.getElementById("loadtime").innerHTML = "Page load time is " + pageLoadTime + " Seconds";
